69df9527392346103fa949b96b2aa7c6ffdca517
Oliver <oliver@worksome.com>
2023-01-02 14:26:24 +0100

    cleanup

--- src/HorizonTelemetryServiceProvider.php
+++ src/HorizonTelemetryServiceProvider.php
@@ -12,16 +12,9 @@ class HorizonTelemetryServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('horizon-telemetry')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_horizon-telemetry_table')
             ->hasCommand(HorizonTelemetryCommand::class);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Worksome\HorizonTelemetry\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
