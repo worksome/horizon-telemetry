7d8dd57740e26076844f64877962738f875f7131
Oliver <oliver@worksome.com>
2023-01-26 16:27:45 +0100

    feat(memory): JIRA-5517 Peak memory usage per job
    Add a peak memory usage histogram that is triggered on job proccesed and is tagged with the job.

---
+++ src/Listeners/ProcessedJobsPeakMemoryUsageListener.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Worksome\HorizonTelemetry\Listeners;
+
+use Illuminate\Queue\Events\JobProcessed;
+use OpenTelemetry\API\Metrics\ObserverInterface;
+use Worksome\HorizonTelemetry\Enums\MeterName;
+use Worksome\HorizonTelemetry\Enums\MeterUnit;
+use Worksome\HorizonTelemetry\MeterProvider;
+
+readonly class ProcessedJobsPeakMemoryUsageListener
+{
+    public function __construct(
+        private MeterProvider $meterProvider,
+    ) {
+    }
+
+    public function __invoke(JobProcessed $event): void
+    {
+        $meter = $this->meterProvider->getMeter(MeterName::ProcessedJobsPeakMemoryUsage);
+
+        $histogram = $meter->createHistogram(
+            MeterName::ProcessedJobsPeakMemoryUsage->value,
+            MeterUnit::Bytes->value,
+            'The memory usage per job',
+        );
+
+
+        $histogram->record(
+            memory_get_peak_usage(),
+            [
+                'name' => $event->job->resolveName(),
+                'queue' => $event->job->getQueue(),
+            ]
+        );
+    }
+}
---
+++ src/Listeners/ProcessedJobsPeakMemoryUsagePreparationListener.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Worksome\HorizonTelemetry\Listeners;
+
+use Illuminate\Queue\Events\JobProcessed;
+use OpenTelemetry\API\Metrics\ObserverInterface;
+use Worksome\HorizonTelemetry\Enums\MeterName;
+use Worksome\HorizonTelemetry\Enums\MeterUnit;
+use Worksome\HorizonTelemetry\MeterProvider;
+
+/**
+ * Needed for cleaning the peak memory usage variable inside PHP between jobs.
+ */
+readonly class ProcessedJobsPeakMemoryUsagePreparationListener
+{
+    public function __invoke(JobProcessed $event): void
+    {
+        memory_reset_peak_usage();
+    }
+}
